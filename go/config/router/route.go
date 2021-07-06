b0220e9c170e2e7ca79c748020063d3527128212
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-07-06 11:17:49 +0800

    route

--- go/config/router/route.go
+++ go/config/router/route.go
@@ -3,13 +3,12 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"nlp/config"
-	"nlp/config/database"
 	"nlp/infrastructure/persistence"
 )
 
 func InitRouter(c *gin.Engine) {
 
-	_config := database.GetMysqlConf()
+	//_config := database.GetMysqlConf()
 	repo, _ := persistence.NewRepositories(config.Conf.MySql.User, config.Conf.MySql.Password, config.Conf.MySql.Port, config.Conf.MySql.Host, config.Conf.MySql.Db)
 	repo.AutoMigrate()
 	RegisterCoreRouter(c, repo)
