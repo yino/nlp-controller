348deabc147704fc5b33f64b0596371ce92b8096
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-07-08 15:27:48 +0800

    pprof

--- config/router/route.go
+++ config/router/route.go
@@ -3,11 +3,15 @@ package router
 import (
 	"github.com/yino/nlp-controller/config"
 	"github.com/yino/nlp-controller/infrastructure/persistence"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(c *gin.Engine) {
+	c.GET("/status", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
 	repo, _ := persistence.NewRepositories(config.Conf.MySql.User, config.Conf.MySql.Password, config.Conf.MySql.Port, config.Conf.MySql.Host, config.Conf.MySql.Db)
 	repo.AutoMigrate()
 	RegisterCoreRouter(c, repo)
--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/yino/nlp-controller/config"
-	"github.com/yino/nlp-controller/config/router"
 	"os"
 
+	"github.com/yino/nlp-controller/config"
+	"github.com/yino/nlp-controller/config/router"
 	_ "github.com/yino/nlp-controller/docs"
 
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
@@ -19,6 +20,7 @@ func main() {
 	os.Setenv("GIN_MODE", "debug")
 	gin.SetMode(gin.DebugMode)
 	app := gin.Default()
+	pprof.Register(app)
 	// 注册路由
 	router.InitRouter(app)
 	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
