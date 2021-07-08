78cc09e9faba95fc717a3b14a1140b68eb853658
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-07-08 11:40:56 +0800

    run swagger

--- go/main.go
+++ go/main.go
@@ -14,14 +14,13 @@ import (
 )
 
 func main() {
-
+	// load config
+	config.GetConf()
 	os.Setenv("GIN_MODE", "debug")
 	gin.SetMode(gin.DebugMode)
 	app := gin.Default()
 	// 注册路由
 	router.InitRouter(app)
 	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	// load config
-	config.GetConf()
 	app.Run(fmt.Sprintf("%s:%s", config.Conf.App.Host, config.Conf.App.Port))
 }
